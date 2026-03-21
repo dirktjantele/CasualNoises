52b2018bd6579fae98050cf76bd7b831ebb77e96
dirktjantele <57864144+dirktjantele@users.noreply.github.com>
2026-03-21 01:47:09 +0100

    Justification settins as an enum

--- Framework/Graphics/Placement/Justification.h
+++ Framework/Graphics/Placement/Justification.h
@@ -14,13 +14,39 @@
 namespace CasualNoises
 {
 
-enum class eJustificationFlags {
+enum class eJustificationFlags  : uint32_t {
+	none				= 0,
+	left 				= 1 << 0,
+	right				= 1 << 1,
+	centred				= 1 << 2,
+	horizontallyCentred	= 1 << 3,
+	verticallyCentred	= 1 << 4,
+  /*
   left = 1 , right = 2 , horizontallyCentred = 4 , top = 8 ,
   bottom = 16 , verticallyCentred = 32 , horizontallyJustified = 64 , centred = 36 ,
   centredLeft = 33 , centredRight = 34 , centredTop = 12 , centredBottom = 20 ,
   topLeft = 9 , topRight = 10 , bottomLeft = 17 , bottomRight = 18
+  */
 };
 
+inline constexpr eJustificationFlags operator| ( eJustificationFlags a, eJustificationFlags b ) {
+    return static_cast <eJustificationFlags> (
+        static_cast <unsigned int>(a) |
+        static_cast <unsigned int>(b)
+    );
+}
+
+inline constexpr eJustificationFlags& operator|= (eJustificationFlags& a, eJustificationFlags b ) {
+    a = a | b;
+    return a;
+}
+/*
+inline constexpr eJustificationFlags operator& ( eJustificationFlags a, eJustificationFlags b ) {
+    return static_cast <eJustificationFlags> (
+        static_cast <unsigned int>(a) &
+        static_cast <unsigned int>(b)
+    );
+*/
 class Justification final
 {
 public:
@@ -31,11 +57,6 @@ public:
 	void setJustificationType (eJustificationFlags flags)
 	{
 		mFlags = flags;
-	}
-
-	void setJustificationType (eJustificationFlags flags1, eJustificationFlags flags2)
-	{
-		mFlags = (eJustificationFlags)((uint32_t)flags1 | (uint32_t)flags2);
 	}
 
 	eJustificationFlags getJustificationType()
